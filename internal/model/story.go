216872c773a7801a4cb4dfa7a618d4595a40016f
João Freitas <1160907@isep.ipp.pt>
2024-09-26 00:08:29 +0100

    refactor(model): segregate cloud and file story models

--- internal/model/story.go
+++ internal/model/story.go
@@ -1,7 +1,18 @@
 package model
 
-type Story struct {
-	Id           string
-	ThumbnailUrl string
-	Url          string
+import "github.com/guackamolly/insta-archiver/internal/data/storage"
+
+// Central model of the application. Defines the required data for an Instagram Story.
+// Typed so it's possible to differentiate a story accessible by clients (string) or accessible in the host machine (os.File)
+type Story[T any] struct {
+	Id        string
+	Username  string
+	Thumbnail T
+	Media     T
 }
+
+// A story model that can be used for client responses.
+type CloudStory = Story[string]
+
+// A story model that lives within the host machine.
+type FileStory = Story[storage.File]
