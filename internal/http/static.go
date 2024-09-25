1550eeda2dcc6e861cf7b5cdd926dbb23500fdbc
João Freitas <1160907@isep.ipp.pt>
2024-09-25 10:39:28 +0100

    feat(server): specify static files to serve

---
+++ internal/http/static.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	serverPublicRootEnvKey = "server_public_root"
+)
+
+var (
+	serverPublicRoot = os.Getenv(serverPublicRootEnvKey)
+
+	files = map[string]string{
+		"/index.html":    serverPublicRoot + "index.html",
+		"/index.css":     serverPublicRoot + "index.css",
+		"/manifest.json": serverPublicRoot + "manifest.json",
+	}
+
+	dirs = map[string]string{
+		"/static": serverPublicRoot + "static/",
+	}
+
+	templates = []string{
+		serverPublicRoot + "archive/index.html",
+	}
+)
+
+func RegisterStaticFiles(e *echo.Echo) error {
+	for k, v := range files {
+		e.File(k, v)
+	}
+
+	for k, v := range dirs {
+		e.Static(k, v)
+	}
+
+	return nil
+}
