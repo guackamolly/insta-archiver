0ab5dd0884c8e2480dcb56b768be93a59228a3ff
João Freitas <1160907@isep.ipp.pt>
2024-09-26 12:34:31 +0100

    feat(server): update archive user stories handler to check for cached views

--- internal/http/sub-handler.go
+++ internal/http/sub-handler.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"github.com/guackamolly/insta-archiver/internal/core"
+	"github.com/guackamolly/insta-archiver/internal/model"
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,19 +10,28 @@ func onArchiveUserStories(
 	ectx echo.Context,
 	vault core.Vault,
 	username string,
-) ArchiveUserStoriesResponse {
+) model.ArchivedUserView {
 	onError := func(err error) error { return onError(ectx, err) }
 
 	pun := core.Invoke(username, vault.PurifyUsername, onError)
+	view := core.Invoke(pun, vault.GetCachedArchivedUserView, onError)
+
+	// If cached, return immediately
+	if view != nil {
+		return *view
+	}
+
 	cs := core.Invoke(pun, vault.GetLatestStories, onError)
 	fs := core.Invoke(cs, vault.DownloadUserStories, onError)
 	cs = core.Invoke(fs, vault.ArchiveUserStories, onError)
 	cs = core.Invoke(cs, vault.PurifyCloudStories, onError)
 
-	return ArchiveUserStoriesResponse{
+	view = &model.ArchivedUserView{
 		Username:    pun,
 		LastStories: cs,
 	}
+
+	return core.Invoke(*view, vault.CacheArchivedUserView, onError)
 }
 
 func onError(
