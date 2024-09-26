80189bd40af2a16310de12099036747f4c3e9a6b
João Freitas <1160907@isep.ipp.pt>
2024-09-26 11:17:21 +0100

    refactor(domain): rename user > story

---
+++ internal/domain/story.go
@@ -0,0 +1,41 @@
+package domain
+
+import (
+	"strings"
+
+	"github.com/guackamolly/insta-archiver/internal/data/repository/user"
+	"github.com/guackamolly/insta-archiver/internal/model"
+)
+
+type GetLatestStories struct {
+	repository user.UserRepository
+}
+
+type PurifyCloudStories struct {
+	physicalContentDir string
+	virtualContentDir  string
+}
+
+func (u GetLatestStories) Invoke(username string) ([]model.CloudStory, error) {
+	return u.repository.Stories(username)
+}
+
+func (u PurifyCloudStories) Invoke(stories []model.CloudStory) ([]model.CloudStory, error) {
+	cs := make([]model.CloudStory, len(stories))
+
+	for i, s := range stories {
+		s.Thumbnail = u.purifyUrl(s.Thumbnail)
+		s.Media = u.purifyUrl(s.Media)
+		cs[i] = s
+	}
+
+	return cs, nil
+}
+
+func (u PurifyCloudStories) purifyUrl(url string) string {
+	if strings.HasPrefix(url, u.physicalContentDir) {
+		return strings.Replace(url, u.physicalContentDir, u.virtualContentDir, 1)
+	}
+
+	return url
+}
