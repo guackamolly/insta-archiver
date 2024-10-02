48cf9fbae17a60474f07edfbabd22644ece15640
João Freitas <1160907@isep.ipp.pt>
2024-10-02 16:07:08 +0100

    fix: wrap map write calls with mutex to prevent concurrent modifications

--- internal/data/storage/mem.go
+++ internal/data/storage/mem.go
@@ -2,17 +2,20 @@ package storage
 
 import (
 	"errors"
+	"sync"
 )
 
 // A [Storage] implementation that uses the process memory as the provider.
 // The input key should implement the [comparable] interface.
 type MemoryStorage[I comparable, O any] struct {
 	cache map[I]O
+	lock  *sync.RWMutex
 }
 
 func NewMemoryStorage[I comparable, O any]() *MemoryStorage[I, O] {
 	return &MemoryStorage[I, O]{
 		cache: map[I]O{},
+		lock:  &sync.RWMutex{},
 	}
 }
 
@@ -27,13 +30,17 @@ func (s *MemoryStorage[I, O]) Lookup(input I) (O, error) {
 }
 
 func (s *MemoryStorage[I, O]) Store(input I, value O) (O, error) {
+	s.lock.Lock()
 	s.cache[input] = value
+	s.lock.Unlock()
 
 	return value, nil
 }
 
 func (s *MemoryStorage[I, O]) Delete(input I) error {
+	s.lock.Lock()
 	delete(s.cache, input)
+	s.lock.Unlock()
 
 	return nil
 }
